3d1154a1e571216203bcdb1896e240270d226c2d
Aleyucra74 <alexandre.yucra@outlook.com>
2019-09-20 11:46:01 -0300

    adding url image to db with api

--- app/Http/Controllers/API/ProdutosController.php
+++ app/Http/Controllers/API/ProdutosController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\API;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Validator;
 use App\Produto;
 
 class ProdutosController extends Controller
@@ -28,13 +29,40 @@ class ProdutosController extends Controller
     public function store(Request $request)
     {
         //
-
-
-        $produto= Produto::create([
-            'nome'=> request('nome'),
-            'descricao'=> request('descricao'),
-            'valor'=> request('valor')
+        $validacao = Validator::make(request()->all(), [
+            'nome' => 'required',
+            'descricao' => 'required',
+            'valor' => 'required'
         ]);
+
+        if($validacao->fails()){
+            
+            return response($validacao->messages(), 401);
+        }
+
+        if (!empty(request('url_imagem'))) {
+            
+            $caminhoAbsoluto = public_path() . '/storage/uploads';
+            #123112.jpg
+            $nomeArquivo = time(). '.' . request('url_imagem')->extension();
+            #movendo para o projeto
+            request('url_imagem')->move($caminhoAbsoluto, $nomeArquivo);
+            
+            $produto= Produto::create([
+                'nome'=> request('nome'),
+                'descricao'=> request('descricao'),
+                'url_imagem'=> $nomeArquivo,
+                'valor'=> request('valor')
+            ]);
+    
+        }else{
+            $produto= Produto::create([
+                'nome'=> request('nome'),
+                'descricao'=> request('descricao'),
+                'valor'=> request('valor')
+            ]);
+
+        }
 
         return response($produto, 201);
     }
